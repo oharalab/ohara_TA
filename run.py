46c4f617cb5bd034665344511cc46a3a3258f4e4
masaya <mysubmail1@gmail.com>
2018-11-16 16:17:59 +0900

    revert

--- run.py
+++ run.py
@@ -16,24 +16,86 @@ import traceback
 import zipfile
 
 from autocompiler import Compiler
+from autocompiler import Makehtml
+
+def read_student_list(student_list):
+    '''students.txtの読み込み'''
+    students = {}
+    with open(student_list) as f:
+        for line in f.readlines():
+            s, n = line.strip().split(' ')
+            students[s] = n
+    return students
+
+def detect_exercise_num(file_path, offset=-1):
+    """
+    課題番号を検出
+    :param file_path: 展開したファイルのパス
+    :param offset:
+    :return: 課題番号. 課題番号がない場合は-1.
+    """
+
+    filename = os.path.split(file_path)[1]
+    if not filename:
+        return -1, None
+
+    match_obj = re.search('(ex)?[0-9]{1,2}_([0-9])\.(\w+)$', filename)
+    if isinstance(match_obj, type(None)):
+        return -1, None
+
+    ex_check = match_obj.group(1) == 'ex'
+    basename = match_obj.group(2)
+    ext = match_obj.group(3)
+    if not file_path.startswith('_'):
+        if re.match(ext, 'c(pp)?') is not None or ext == 'pptx':
+            if not ex_check:
+                print('Warning: File does not starts with "ex". {}'.format(filename))
+            exercise_num = int(basename)
+            return exercise_num + offset, ex_check
+        else:
+            return -1, ex_check
+
+
+def get_latest_program_info(program_info):
+    valid = False
+    name_checks = [program_info[i]['name_check'] for i in range(len(program_info))]
+    timestamps = [program_info[i]['timestamp'] for i in range(len(program_info))]
+
+    idx = 0
+    for i, (n, t) in enumerate(zip(name_checks, timestamps)):
+        if valid and not n:
+            continue
+        elif not valid and n:
+            valid = n
+            idx = i
+        else:
+            if timestamps[idx] < timestamps[i]:
+                idx = n
+
+    return program_info[idx]
+
 
 
 def main(args=None):
+    if sys.platform == 'cygwin':
+        NKF_BIN = './bin/nkf32.exe'
+    else:
+        NKF_BIN = 'nkf'
     """
 
     :param args:
     :return:
     """
-    students = {}
-    with open(args.students) as f:
-        for line in f.readlines():
-            s, n = line.strip().split(' ')
-            students[s] = n
 
+    students = read_student_list(args.students)
+
+    #ディレクトリexercise/ex%dの設定
     exercise_path = args.exercise_path % args.exercise
+    #ディレクトリexercise/ex%d/answersの設定
     answers_path = os.path.join(exercise_path, 'answers')
+    #ディレクトリexercise/ex%d/sentencesの設定
     exercise_sentences_path = os.path.join(exercise_path, 'sentences')
-
+    #zipファイルが有るか確認、なければエラー
     exercise_sentences_files = glob.glob(os.path.join(exercise_sentences_path, '*'))
     if len(exercise_sentences_files) == 1:
         filename = exercise_sentences_files[0]
@@ -136,115 +198,7 @@ def main(args=None):
                     traceback.print_exc()
                     print('原因不明')
 
-        prev, program_info = None, None
-        with open(os.path.join(args.output_dir, '{}_{}.html'.format('out', student_id)), 'w') as out_file:
-            out_file.write(
-                '<!DOCTYPE html>\n'
-                '<html lang="ja">\n'
-            )
-
-            out_file.write(HEADER.format(student_id))
-            out_file.write('<body>\n')
-            out_file.write('<header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">'
-                           '</header>')
-            out_file.write('<div class="container">\n')
-            out_file.write('<div class="row">\n')
-            out_file.write('<div class="col-md-2">\n')
-            out_file.write('<div class="sidebar content-box">\n')
-            out_file.write('<ul class="nav">\n')
-            for sid in student_ids:
-                out_file.write('<li class="{1}">'
-                               '<a href="out_{0}.html">{0}</a>'
-                               '</li>\n'.format(sid, 'current' if sid == student_id else ''))
-            out_file.write('</ul>\n')
-            out_file.write('</div>\n')
-            out_file.write('</div>\n')
-            out_file.write('<div class="col-md-10">\n')
-            out_file.write('<div class="row">\n')
-            out_file.write('<div class="col-md-8">\n')
-            out_file.write('<h3>{} {}</h3>\n'.format(student_id, students[student_id]))
-            out_file.write('</div><!-- col-md-8 -->\n')
-            out_file.write('<div class="col-md-4">\n')
-            for path in sorted(glob.glob(os.path.join(exercise_sentences_path, '**'), recursive=True)):
-                if re.match('ex[0-9]+_[0-9]+.html', os.path.basename(path)):
-                    name = os.path.splitext(os.path.basename(path))[0]
-                    out_file.write(MODAL.format(name, read_html_body(path)))
-            out_file.write('</div><!-- col-md-4 -->\n')
-            out_file.write('</div><!-- row -->\n')
-
-            # tab
-            out_file.write('<ul class="nav nav-tabs">\n')
-            for i in range(len(compilers)):
-                out_file.write(
-                    '<li class="nav-item">\n'
-                    '<a href="#ex{0}" class="nav-link {1}" data-toggle="tab">Exercise {0}</a>\n'
-                    '</li>\n'.format(i+1, '' if i > 0 else 'active')
-                )
-            out_file.write('</ul>\n')
-            out_file.write('<div class="tab-content">\n')
-            for i, compiler in enumerate(compilers):
-                ret = False
-                results = {}
-
-                if i > 0:
-                    out_file.write('</div>  <!-- tab{}-->\n'.format(i))
-                out_file.write('<div id="ex{}" class="tab-pane {}">\n'.format(i+1, '' if i > 0 else 'active'))
-                try:
-                    if verify.get(i) is None:
-                        write_not_submitted(out_file)
-                        continue
-                    if i > 0:
-                        prev = program_info
-                    program_info = get_latest_program_info(verify[i])
-
-                    # 時間の判定
-                    if prev is not None:
-                        if prev['timestamp'] < program_info['timestamp']:
-                            program_info['timestamp_status'] = 'OK'
-                        else:
-                            program_info['timestamp_status'] = 'NG'
-                    else:
-                        program_info['timestamp_status'] = 'OK'
-
-                    # コードの内容をファイルに書き出す
-                    ret, msg = compiler.convert_encoding(program_info['file_path'])
-                    if not ret:
-                        print('Cannot convert encoding')
-
-                    # 対象ソースコードをコンパイル
-                    ret, msg = compiler.compile(program_info['file_path'], student_id)
-                    if not ret:
-                        saiten['ex%d' % (i + 1)] = 2
-                        program_info['status'] = 'NG (Compile Error)'
-                    else:
-                        # 対象プログラムを実行
-                        results = compiler.execute(student_id)
-                        saiten['ex%d' % (i + 1)] = 5
-                        program_info['status'] = 'OK' if program_info['name_check'] else 'NG (Naming rule)'
-                except Exception as e:
-                    print(e)
-                    traceback.print_exc()
-                    program_info['status'] = 'NG (Naming rule)'
-                finally:
-                    pass
-
-                write_program_info(program_info, out_file)
-                write_code(program_info['file_path'], out_file)
-                if ret:
-                    write_trials(results, answers, i, out_file)
-                else:
-                    write_error_msg(msg, out_file)
-
-            scoring[student_id] = saiten
-            out_file.write('</div>\n')
-            out_file.write('</div> <!-- div.tab-content -->\n')
-            out_file.write('</div> <!-- div.col-md-10 -->\n')
-            out_file.write('</div> <!-- div.row -->\n')
-            out_file.write('</div> <!-- div.container -->\n')
-
-            out_file.write(FOOTER)
-            out_file.write('</body>')
-            out_file.write('</html>')
+        Makehtml.create_html(args.output_dir, student_id)
 
     print('Complete!')
 
@@ -256,9 +210,11 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--num_tasks', help='number of tasks', default=4, type=int)
     parser.add_argument('-d', '--debug', help='debug mode', default=False, type=bool)
     parser.add_argument('-o', '--output_dir', help='output directory', default='./output')
-    parser.add_argument('-e', '--exercise', help='', default=4, type=int)
+    parser.add_argument('-e', '--exercise', help='number of excrcises', type=int)
     parser.add_argument('--exercise_path', help='', default='./exercise/ex%d')
     parser.add_argument('-s', '--students', help='', default='./students.txt')
+    parser.add_argument('-l', '--timeout', help='', default=1, type=int)
+    parser.add_argument('-gcc','--gcc_path', help='path to gcc', default='/usr/bin/gcc')
 
     args = parser.parse_args()
 
