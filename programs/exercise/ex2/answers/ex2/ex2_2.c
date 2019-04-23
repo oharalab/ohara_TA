ef3f3d05ec8478bb9218787dff713242728c1a52
jackee777 <stardustchronicle10030709@gmail.com>
2019-04-23 15:48:22 +0900

    modify unnecesary files

--- programs/exercise/ex2/answers/ex2/ex2_2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-  printf("情報テクノロジー学科\n三國孔明\n");
-
-  return 0;
-}
--- programs/p1_1/correct.c
+++
@@ -1,9 +0,0 @@
-/* p1_1 */
-#include <stdio.h>
-
-int main() {
-  printf("青山学院大学\n");
-  printf("青山花子\n");
-
-  return 0;
-}
