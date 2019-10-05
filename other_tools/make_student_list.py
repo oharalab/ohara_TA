89bb621a8c73d61e4d2e361ecf2fbea5d6e99c45
jackee777 <stardustchronicle10030709@gmail.com>
2019-10-05 19:43:33 +0900

    chanege friday compiler
    friday のフォルダのフォーマットに対応

--- other_tools/make_student_list.py
+++ other_tools/make_student_list.py
@@ -1,3 +1,4 @@
+"""
 import pandas as pd
 
 data_frame = pd.read_csv("student.csv", encoding="cp932", skiprows=2)
@@ -8,4 +9,15 @@ print(data_frame["(氏名)"])
 with open("student_temp.txt", "w") as f:
     for number, name in zip(data_frame["(学生番号/教職員番号)"], data_frame["(氏名)"]):
         f.write(str(number) + " " + name + "\n")
+"""
+
+with open("student.csv", "r", encoding = "cp932") as fr:
+    # skip rows
+    for i in range(3):
+        fr.readline()
+    with open("students.txt", "w") as fw:
+        for line in fr:
+            student_info = line.split(",")
+            fw.write(str(student_info[3]) + " " + student_info[4] + "\n")
+    
 
